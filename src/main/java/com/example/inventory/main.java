45ac41f00dbc05ca7327bb5d700c2deca3afba0e
unknown <manushreebartaria07@gmail.com>
2025-08-31 02:17:11 +0530

    adding routes

--- src/main/java/com/example/inventory/main.java
+++ src/main/java/com/example/inventory/main.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+class Inventory {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Inventory.class, args);
 	}
 
 }
